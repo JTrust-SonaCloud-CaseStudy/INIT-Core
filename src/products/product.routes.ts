4963cfe51af2f9bc448ab574a0512baf82a694bf
Sokea Aing <Sokea.Aing@jtrustroyal.com>
2024-04-18 09:41:36 +0700

    chore: remove unused import

--- src/products/product.routes.ts
+++ src/products/product.routes.ts
@@ -1,5 +1,4 @@
 import express, {Request, Response} from "express"
-import { Product, UnitProduct } from "./product.interface"
 import * as database from "./product.database"
 import {StatusCodes} from "http-status-codes"
 
